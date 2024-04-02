0fb689e01de62aa1df091ec176c7ed2217a73ed4
Sean Mitchell <seanrmitchell211@gmail.com>
2024-04-01 22:03:39 -0400

    Intial Commit
    Adds:
    Agents and Map
    Camera Changes

---
+++ Ai-Pathfinding-Research/Assets/Scripts/MoveToTarget.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToTarget : MonoBehaviour
+{
+    public Transform target;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.SetDestination(target.position);
+    }
+}
