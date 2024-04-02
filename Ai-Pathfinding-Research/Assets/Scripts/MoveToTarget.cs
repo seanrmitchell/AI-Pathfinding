d3e418ad6b04eb8d0e3f76e0997cfd21926fdce1
Sean Mitchell <seanrmitchell211@gmail.com>
2024-04-02 17:17:36 -0400

    Timer & Target Set Up

--- Ai-Pathfinding-Research/Assets/Scripts/MoveToTarget.cs
+++ Ai-Pathfinding-Research/Assets/Scripts/MoveToTarget.cs
@@ -7,10 +7,22 @@ public class MoveToTarget : MonoBehaviour
 {
     public Transform target;
     public NavMeshAgent agent;
+    public NavMeshPath path;
+    public Timer timer;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         agent.SetDestination(target.position);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            agent.isStopped = true;
+            timer.isTiming = false;
+        }
     }
 }
--- Ai-Pathfinding-Research/Assets/Scripts/Timer.cs
+++ Ai-Pathfinding-Research/Assets/Scripts/Timer.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI timeText;
+    public bool isTiming;
+
+    private float time;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isTiming)
+        {
+            time += Time.deltaTime;
+            timeText.text = time.ToString("000.00");
+        }
     }
 }
